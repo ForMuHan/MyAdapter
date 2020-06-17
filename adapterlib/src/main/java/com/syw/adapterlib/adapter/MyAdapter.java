1721e2da3762960a85b9e2e287e43186c135e44f
formuhan <syw1034455910@163.com>
2020-06-17 17:15:44 +0800

    修改点击事件

--- adapterlib/src/main/java/com/syw/adapterlib/adapter/MyAdapter.java
+++ adapterlib/src/main/java/com/syw/adapterlib/adapter/MyAdapter.java
@@ -20,36 +20,26 @@ public class MyAdapter<T> extends RecyclerView.Adapter<ViewHolder> {
      */
     private int layoutId;
     /**
-     *  布局文件data中变量的BR值
+     * 布局文件data中变量的BR值
      */
     private int brId;
     /**
      * item中某个view的id
      */
-    private int viewId;
+    private int[] viewIds;
 
-    private OnItemClickListener listener;
     private OnViewClickListener viewListener;
 
     private ViewHolder viewHolder;
 
     /**
-     * 整个item的点击事件
-     *
-     * @param listener
-     */
-    public void setOnItemClickListener(OnItemClickListener listener) {
-        this.listener = listener;
-    }
-
-    /**
      * item中某个view的点击事件
      *
-     * @param viewId   点击view的id
+     * @param viewIds  点击view的id
      * @param listener 点击的监听回调
      */
-    public void setOnViewClickListener(int viewId, OnViewClickListener listener) {
-        this.viewId = viewId;
+    public void setOnViewClickListener(int[] viewIds, OnViewClickListener listener) {
+        this.viewIds = viewIds;
         this.viewListener = listener;
     }
 
@@ -81,18 +71,15 @@ public class MyAdapter<T> extends RecyclerView.Adapter<ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.getBinding().setVariable(brId, mDatas.get(position));
         holder.getBinding().executePendingBindings();
-        holder.itemView.setOnClickListener(v -> {
-            if (listener != null) {
-                listener.onItemClick(holder.itemView, holder.getAdapterPosition());
-            }
-        });
         try {
-            View view = holder.getBinding().getRoot().findViewById(viewId);
-            view.setOnClickListener(v -> {
-                if (viewListener != null) {
-                    viewListener.onViewClick(holder.itemView, holder.getAdapterPosition());
-                }
-            });
+            for (int i = 0; i < viewIds.length; i++) {
+                View view = holder.getBinding().getRoot().findViewById(viewIds[i]);
+                view.setOnClickListener(v -> {
+                    if (viewListener != null) {
+                        viewListener.onViewClick(view, holder.getAdapterPosition());
+                    }
+                });
+            }
         } catch (Exception e) {
 
         }
@@ -101,11 +88,6 @@ public class MyAdapter<T> extends RecyclerView.Adapter<ViewHolder> {
     @Override
     public int getItemCount() {
         return mDatas == null ? 0 : mDatas.size();
-    }
-
-
-    public interface OnItemClickListener {
-        void onItemClick(View view, int position);
     }
 
     public interface OnViewClickListener {
--- app/src/main/java/com/syw/adapter/MainActivity.java
+++ app/src/main/java/com/syw/adapter/MainActivity.java
@@ -4,14 +4,11 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
 
-
 import com.syw.adapter.databinding.ActivityMainBinding;
 import com.syw.adapterlib.adapter.MyAdapter;
-import com.syw.adapter.BR;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,17 +28,22 @@ public class MainActivity extends AppCompatActivity {
             testBeans.add(new TestBean("i1:" + i, "i2:" + i));
         }
         adapter.setDatas(testBeans);
-        adapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(View view, int position) {
-                Toast.makeText(MainActivity.this, "position:" + position, Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        adapter.setOnViewClickListener(R.id.text1, new MyAdapter.OnViewClickListener() {
+        adapter.setOnViewClickListener(new int[]{R.id.parent, R.id.text1, R.id.text2}, new MyAdapter.OnViewClickListener() {
             @Override
             public void onViewClick(View view, int position) {
-                Toast.makeText(MainActivity.this, "view:" + testBeans.get(position).getText1(), Toast.LENGTH_SHORT).show();
+                switch (view.getId()) {
+                    case R.id.parent:
+                        Toast.makeText(MainActivity.this, "parent:" + position, Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.text1:
+                        Toast.makeText(MainActivity.this, "text1:" + position, Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.text2:
+                        Toast.makeText(MainActivity.this, "text2:" + position, Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
             }
         });
 
